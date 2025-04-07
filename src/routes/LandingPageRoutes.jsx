fd4e355c0c23478e56c320c4d5e128dec1597091
6ankit <ankitlekharajani752@gmail.com>
2025-04-07 13:42:24 +0530

    Added updates after cloning

--- src/routes/LandingPageRoutes.jsx
+++ src/routes/LandingPageRoutes.jsx
@@ -1,21 +1,25 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Submain from "./components/subscription/submain.jsx";
-import HeroSection from "./components/hero/HeroSection.jsx";
-import Review from "./components/reviewpage/review";
-import AuthForm from "./components/Signup Code/signup.jsx";
-import WalletSection from "./components/wallet/Wallet_section.jsx";
-import UpdateAdminDetails from "./components/Admin/Update-details.jsx";
-import OrderTracking from "./components/User_order/OrderTracking.jsx";
-import Dashboard from "./components/super_admin/dashboard/Dashboard.jsx";
-import CloudRegistration from "./components/super_admin/kitchen_reg/CloudRegistration.jsx";
-import AdminDetails from "./components/super_admin/AdminDetails/AdminDetails.jsx";
-import ManagerDetail from "./components/super_admin/AdminDetails/ManagerDetail.jsx";
-import Menu from "./components/explore_menu/Menu.jsx";
-// import SignUp from "./components/signin/signup.jsx"
-// import Cuisines from "./components/popular_cusine/cuisines.js"
 
-function App() { 
+// Page
+import Home from "..Landing page/pages/Home";
+
+// Components inside "Landing page"
+import Submain from "../Landing page/components/subscription/submain";
+import HeroSection from "../Landing page/components/hero/HeroSection";
+import Review from "../Landing page/components/reviewpage/review";
+import AuthForm from "../Landing page/components/Signup Code/signup";
+import WalletSection from "../Landing page/components/wallet/Wallet_section";
+import UpdateAdminDetails from "../Landing page/components/Admin/Update-details";
+import OrderTracking from "../Landing page/components/User_order/OrderTracking";
+import Dashboard from "../Landing page/components/super_admin/dashboard/Dashboard";
+import CloudRegistration from "../Landing page/components/super_admin/kitchen_reg/CloudRegistration";
+import AdminDetails from "../Landing page/components/super_admin/AdminDetails/AdminDetails";
+import ManagerDetail from "../Landing page/components/super_admin/AdminDetails/ManagerDetail";
+import Menu from "../Landing page/components/explore_menu/Menu";
+
+
+function LandingPageRoutes() { 
   return (
     <Router>
       <Routes>
@@ -42,4 +46,4 @@ function App() {
   );
 }
 
-export default App;
+export default LandingPageRoutes;
